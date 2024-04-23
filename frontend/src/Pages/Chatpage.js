bb085dd900cd733e325b912bdf9e0f56b4877966
Cuong0211 <cuongdeptrai9823@gmail.com>
2024-04-23 23:03:43 +0700

    call first api, install axios, use lifecycle react but not complete

--- frontend/src/Pages/Chatpage.js
+++ frontend/src/Pages/Chatpage.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import axios from 'axios'
 
 const Chatpage = () => {
-  return <div>Chat page</div>;
+   const fethChats = async () => {
+     const data = await axios.get("/api/chat");
+     console.log(data);
+};
 
-}
+return <div>Chat page</div>;
+};
 
 export default Chatpage
