bd12b4538fede714669a164d36f6fde4012887af
Cuong0211 <cuongdeptrai9823@gmail.com>
2024-04-23 21:13:36 +0700

    New react router dom and create Webpage, Chatpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import { Route } from 'react-router-dom';
+import Homepage from './Pages/Homepage';
+import Chatpage from './Pages/Chatpage';
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" />
-      <Route path="/chats" />
+      <Route path="/" component={Homepage} exact />
+      <Route path="/chats" component={Chatpage}/>
     </div>
     
   );
---
+++ frontend/src/Pages/Chatpage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Chatpage = () => {
+  return <div>Chat page</div>;
+
+}
+
+export default Chatpage
