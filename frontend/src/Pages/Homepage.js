b13b8d5d5753244bf319936aa414012a78222752
Cuong0211 <cuongdeptrai9823@gmail.com>
2024-04-28 16:32:53 +0700

    Css for web and add container, box form chakra ui

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Container, Box, Text } from "@chakra-ui/react";
 
 const Homepage = () => {
-  return <div>Home</div>;
-}
+  return (
+    <Container maxW="x1" centerContent>
+      <Box>
+        <Text>Talk-a-Tive</Text>
+      </Box>
+    </Container>
+  );
+};
 
-export default Homepage
+export default Homepage;
