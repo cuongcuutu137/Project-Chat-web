d1a6ac800b38ac37b54f6ea5bd907120b49eca1a
Cuong0211 <cuongdeptrai9823@gmail.com>
2024-04-28 23:10:19 +0700

    styling for box talk-a-tive

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -4,9 +4,22 @@ import { Container, Box, Text } from "@chakra-ui/react";
 const Homepage = () => {
   return (
     <Container maxW="x1" centerContent>
-      <Box>
-        <Text>Talk-a-Tive</Text>
+      <Box
+        d="flex"
+        justifyContent="center"
+        textAlign="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="1g"
+        borderWidth="1px"
+      >
+        <Text fontSize="4x1" fontFamily="Work sans" color="black">
+          Talk-A-Tive
+        </Text>
       </Box>
+      <Box></Box>
     </Container>
   );
 };
