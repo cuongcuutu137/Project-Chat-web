a0f6da86861c453c968e6140886115891cc8e297
Cuong0211 <cuongdeptrai9823@gmail.com>
2024-05-02 23:21:09 +0700

    Create sign up and login box, complete

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Container, Box, Text } from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
 const Homepage = () => {
   return (
-    <Container maxW="x1" centerContent>
+    <Container maxW="xl" centerContent>
       <Box
         d="flex"
         justifyContent="center"
@@ -12,14 +15,36 @@ const Homepage = () => {
         bg={"white"}
         w="100%"
         m="40px 0 15px 0"
-        borderRadius="1g"
+        borderRadius="lg"
         borderWidth="1px"
       >
-        <Text fontSize="4x1" fontFamily="Work sans" color="black">
+        <Text fontSize="4xl" fontFamily="Work sans" color="black">
           Talk-A-Tive
         </Text>
       </Box>
-      <Box></Box>
+      <Box
+        bg="white"
+        w="100%"
+        p={4}
+        borderRadius="lg"
+        color="black"
+        borderWidth="1px"
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="100%">Login</Tab>
+            <Tab width="100%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
     </Container>
   );
 };
