136945a093041b1f84bf7005a1c6036b26ba452b
Trinh Bac <trinhgiachang@gmail.com>
2024-05-03 23:45:01 +0700

    update code

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
+const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.log(`Error: ${error.message}`.trimEnd.bold);
+    console.log(`Error: ${error.message}`.red.bold);
     process.exit();
   }
 };
