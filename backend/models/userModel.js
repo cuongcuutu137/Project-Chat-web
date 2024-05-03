b0e34d2d30ddb5c61c500a9ec6bb9dbd5787de9d
Cuong0211 <cuongdeptrai9823@gmail.com>
2024-05-04 00:05:37 +0700

    updated code

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    chat: { type: String, required: true },
+    password: { type: String, required: true },
     pic: {
       type: String,
       default:
