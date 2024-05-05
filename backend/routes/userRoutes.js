2d1c689de60af3339aea1610e5651518aad3f4c8
Cuong0211 <cuongdeptrai9823@gmail.com>
2024-05-05 18:55:28 +0700

    searching-user-api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,14 @@
 const express = require("express");
-const { registerUser, authUser } = require("../controllers/userControllers");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers");
+
 const router = express.Router();
 
+router.route("/").get(allUsers);
 router.route("/").post(registerUser);
 router.post("/login", authUser);
+
 module.exports = router;
