f66fbbe7df282ed2308150d40a010e9f98cc3ac3
Cuong0211 <cuongdeptrai9823@gmail.com>
2024-05-11 17:44:13 +0700

    updated code
    (cherry picked from commit 92058eb597b00836be6750b46efb954d7ea89b89)

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -1,7 +1,24 @@
 import React from "react";
-
-const ChatBox = () => {
-  return <div>Chat Box</div>;
+import { ChatState } from "../Context/ChatProvider";
+import { Box } from "@chakra-ui/layout";
+import SingleChat from "./SingleChat";
+const ChatBox = ({ fetchAgain, setFetchAgain }) => {
+  const { selectedChat } = ChatState();
+  return (
+    <Box
+      display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+      alignItems="center"
+      flexDir="column"
+      padding={3}
+      background="white"
+      width={{ base: "100%", md: "68%" }}
+      borderRadius="lg"
+      borderWidth="1px"
+      ml="20px"
+    >
+      <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+    </Box>
+  );
 };
 
 export default ChatBox;
